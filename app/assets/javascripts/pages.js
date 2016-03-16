4cb65bb48b99c799113e85067f33733c25207be0
Cyril Chehovah <c.chehovah@gmail.com>
2016-03-16 12:21:20 -0700

    adding some css + modal +  javascript

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -13,4 +13,32 @@ $(function() {
       }
     }
   });
+
+  $("div[class='navbar-logo']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  })
+
+  function reposition() {
+      var modal = $(this),
+          dialog = modal.find('.modal-dialog');
+      modal.css('display', 'block');
+
+      // Dividing by two centers the modal exactly, but dividing by three
+      // or four works better for larger screens.
+      dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+  }
+  // Reposition when a modal is shown
+  $('.modal').on('show.bs.modal', reposition);
+  // Reposition when the window is resized
+  $(window).on('resize', function() {
+      $('.modal:visible').each(reposition);
+  });
+
 });
+
+
