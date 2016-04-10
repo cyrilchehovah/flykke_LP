9f2bcf5d9c4815c597216242fea9a01bff1ec6f0
Cyril Chehovah <c.chehovah@gmail.com>
2016-04-10 09:01:22 -0700

    debeug encore

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   after_create :send_welcome_email
   after_create :register_user_to_mailchimp_list
 
-  # def name
-  #   email
-  # end
+  def name
+    email
+  end
 
   # def new
   #   @user = User.new(params)
@@ -21,14 +21,14 @@ class User < ActiveRecord::Base
   #   end
   # end
 
-  # private
+  private
 
-  # def send_welcome_email
-  #   UserMailer.welcome(self).deliver_now
-  # end
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 
-  # def register_user_to_mailchimp_list
-  #   SubscribeToNewsletter.new.run(self.email)
-  # end
+  def register_user_to_mailchimp_list
+    SubscribeToNewsletter.new.run(self.email)
+  end
 
 end
